d4eb2cd1c8300f41625f6c63df8557d9f768b72f
naufalfalaqi <89500573+naufalfalaqi@users.noreply.github.com>
2021-08-25 17:48:47 +0700

    Add files via upload

---
+++ tugas1.m
@@ -0,0 +1,210 @@
+halaman 22
+1. Hitunglah dengan MATLAB:
+>> 12 / 3,5
+
+ans =
+
+     4
+
+
+ans =
+
+     5
+>> (3 + 5/4)^2
+ 
+
+ans =
+
+   18.0625
+>> (0.25^2 + 0.75^2)^1/2
+
+ans =
+
+    0.3125
+>> 2 / (6/0.3)
+
+ans =
+
+    0.1000
+    
+2. Buatlah empat variabel berikut:
+A = 25 B = 50 C = 125 D = 89
+Hitunglah dan simpan dalam variabel baru:
+X = A + B +C Y = A / (D+B)
+Z = DA/B + C
+  jawaban :
+>> a = 25 ;
+>> b = 50 ;
+>> c = 125 ;
+>> d = 89 ;
+
+>> X = a+b+c
+
+X =
+
+   200
+>> Y = a/(d+b)
+
+Y =
+
+    0.1799
+>>Z = d^a/b+c
+
+Z =
+
+  1.0859e+047
+
+3. Manakah di antara nama-nama variabel berikut yang valid ?
+luas, kel_1, 2_data, diff:3, Time, time_from_start,10_hasil_terakhir, nilai-awal
+ jawaban :
+>> luas = 21
+
+luas =
+
+    21
+
+>>  kel_1 = 21
+
+kel_1 =
+
+    21
+>> Time = 21
+
+Time =
+
+    21
+
+>> time_from_start = 21
+
+time_from_start =
+
+    21
+
+4. Misalkan: x = pi/6, y = 0,001; hitunglah:
+sqrt(y),exp(x),sin x, cos 2x, tan 3x,log10 y, log2 y, 1n y.
+ jawaban :
+>> x = pi/6 ;
+>> y = 0,001 ;
+>> sqrt (y)
+
+ans =
+
+     0
+
+>> exp (-x)
+
+ans =
+
+    0.5924
+>> sin (x)
+
+ans =
+
+    0.5000
+>> cos 2x
+
+ans =
+
+    0.9650    0.8142
+
+>> tan 3x
+
+ans =
+
+    0.9031    0.7131
+
+5. Misalkan: p = 9+16i dan q = -9+16i; hitunglah:
+r = pq, s = p/q, p-r, r+s, p^2, ?p, 
+|p|, angle p, |q|, angle q, |r|, angle r, |s|, angle s.
+ jawaban :
+>> p = 9+16i ;
+>> q = -9+16i ;
+>> r= p*q
+
+r =
+
+  -337
+
+>> s=p/q
+
+s =
+
+   0.5193 - 0.8546i
+>> p-r
+
+ans =
+
+  3.4600e+002 +1.6000e+001i
+
+>> r+s
+
+ans =
+
+ -3.3648e+002 -8.5460e-001i
+
+>> p^2
+
+ans =
+
+ -1.7500e+002 +2.8800e+002i
+>> sqrt (q)
+
+ans =
+
+   2.1630 + 3.6985i
+
+>> abs (p)
+
+ans =
+
+   18.3576
+
+>> angle (p)
+
+ans =
+
+    1.0584
+
+>> abs (q)
+
+ans =
+
+   18.3576
+
+>> angle (q)
+
+ans =
+
+    2.0832
+
+>> abs (r)
+
+ans =
+
+   337
+
+>> abs (r)
+
+ans =
+
+   337
+
+>> angle (r)
+
+ans =
+
+    3.1416
+
+>> abs (s)
+
+ans =
+
+     1
+
+>> angle (s)
+
+ans =
+
+   -1.0248
+   
+halaman
